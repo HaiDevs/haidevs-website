b390ee06f9db01b562ef87f835a4541828ae70e1
Nick Rameau <hello@nick.ht>
2022-09-05 15:36:27 -0400

    Set up Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  // fonts: {
+  //   body: "Manrope, sans-serif",
+  //   heading: "Manrope, sans-serif",
+  // },
+  styles: {
+    global: {
+      body: {
+        bg: "#000",
+      },
+
+      // To hide the default focus outline on elements that are keyboard focusable
+      '*:focus': {
+        shadow: 'none !important',
+      },
+    }
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
