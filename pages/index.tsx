b43dcfb1c00079177f1cdccc872f3a6d359375ea
Nick Rameau <hello@nick.ht>
2022-09-08 20:42:03 -0400

    Set up Pageable for the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import { Flex, Heading, Text } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Script from 'next/script'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
+  // To prevent the section contents from rendering before the script is loaded
+  const [isInit, setIsInit] = useState(false)
+
   return (
     <>
       <Head>
@@ -12,25 +17,72 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Flex
-        align="center"
-        justify="center"
-        direction="column"
-        mt="50px"
-      >
-        <Heading fontSize="24px">
-          🇭🇹 HaiDevs
-        </Heading>
+      <Script
+        src="https://unpkg.com/pageable@latest/dist/pageable.min.js"
+        onLoad={() =>
+          // @ts-ignore - pageable is not a module
+          new Pageable("#container", {
+            freeScroll: true,
+            onInit() {
+              setIsInit(true)
+            }
+          })
+        }
+      />
 
-        <Heading
-          fontWeight="700"
-          fontSize="48px"
-          w="50%"
-          textAlign="center"
-          mt="15px"
+      <Flex
+        id="container"
+      >
+        <Flex
+          data-anchor="hero"
+          align="center"
+          justify="center"
+          direction="column"
         >
-          Pi gwo kominote dijital<br />Devlopè Ayisyen<br />nan mond lan.
-        </Heading>
+          {isInit && (
+            <>
+              <Heading fontSize="24px">
+                🇭🇹 HaiDevs
+              </Heading>
+
+              <Heading
+                fontWeight="700"
+                fontSize="48px"
+                w="50%"
+                textAlign="center"
+                mt="15px"
+              >
+                Pi gwo kominote dijital<br />Devlopè Ayisyen<br />nan mond lan.
+              </Heading>
+            </>
+          )}
+        </Flex>
+
+        <Flex
+          data-anchor="hero-2"
+          align="center"
+          justify="center"
+          direction="column"
+        >
+          {isInit && (
+            <>
+              <Heading fontSize="24px">
+                🇭🇹 HaiDevs
+              </Heading>
+
+              <Heading
+                fontWeight="700"
+                fontSize="48px"
+                w="50%"
+                textAlign="center"
+                mt="15px"
+              >
+                Pi gwo kominote dijital<br />Devlopè Ayisyen<br />nan mond lan.
+              </Heading>
+            </>
+          )}
+        </Flex>
+
       </Flex>
     </>
   )
