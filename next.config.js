3597ba2d6ce902eb051bbd0b383ee1f5a0e5e653
Joas Sanon <self@joassanon.me>
2023-03-16 20:54:00 -0400

    Added PWA config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV == 'development',
+	register: true,
+	skipWaiting: true,
+	scope: '/',
+	cacheStartUrl: true,
+	dynamicStartUrl: true,
+	reloadOnOnline: true
+});
+
+const nextConfig = withPWA({
+	reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-}
+	poweredByHeader: false,
+	onDemandEntries: {
+		// period (in ms) where the server will keep pages in the buffer
+		maxInactiveAge: 25 * 1000,
+		// number of pages that should be kept simultaneously without being disposed
+		pagesBufferLength: 20,
+	},
+	basePath: '/'
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
