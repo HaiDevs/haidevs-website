fdffbb857df82f2f1f7948782b7bf66446bed56f
Joas Sanon <msejoas@gmail.com>
2023-03-19 00:35:43 -0400

    Pwa Configuration (#30)
    * Added the menu component back
    
    * Added next-pwa package and took away package-lock.json to use yarn.lock
    
    * Added PWA config to next.config.js
    
    * Placeholder logo
    
    * Icons generation
    
    * Added necessary link and meta tags
    
    * Icon configuration for Edge
    
    * Added Pwa Manifest file a duplicate of manifest.json for modern broswer support
    
    * Unintended configuration removal from next.config.js
    
    * Git ignore for next-pwa generated files
    
    * Next-pwa configuration test passed:  Add PWA Configuration in next.config.js with next-pwa and pwa-asset-generator #28
    
    * Get rid of i18n config
    
    ---------
    
    Co-authored-by: Nick Rameau <hello@nick.ht>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
-import { Flex, Heading, Text } from '@chakra-ui/react'
-import type { NextPage } from 'next'
-import { NextSeo } from 'next-seo'
-import Head from 'next/head'
+import { Flex, Heading, Text } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import { NextSeo } from "next-seo";
+import Head from "next/head";
 import { SocialMediaBox } from "@components/SocialMediaBox";
-import { FaTwitter, FaDiscord } from 'react-icons/fa'
+import { FaTwitter, FaDiscord } from "react-icons/fa";
 
 // NOTE: if using fullpage extensions/plugins put them here and pass it as props.
 const pluginWrapper = () => {
   /*
-  * require('../static/fullpage.scrollHorizontally.min.js'); // Optional. Required when using the "scrollHorizontally" extension.
-  */
+   * require('../static/fullpage.scrollHorizontally.min.js'); // Optional. Required when using the "scrollHorizontally" extension.
+   */
 };
 
 const Home = () => {
@@ -22,6 +22,7 @@ const Home = () => {
   return (
     <div className="App">
       <NextSeo title="Home" />
+      <Menu />
 
       <ReactFullpage
         credits={{ enabled: false }}
@@ -30,41 +31,45 @@ const Home = () => {
         pluginWrapper={pluginWrapper}
         render={() => (
           <ReactFullpage.Wrapper>
-            <Flex className="section fp-noscroll" align="center" justify="center" textAlign="center" overflow="hidden">
-              <Heading fontSize="32px">
-                🇭🇹 HaiDevs
-              </Heading>
+            <Flex
+              className="section fp-noscroll"
+              align="center"
+              justify="center"
+              textAlign="center"
+              overflow="hidden"
+            >
+              <Heading fontSize="32px">🇭🇹 HaiDevs</Heading>
 
-              <Heading
-                fontSize="52px"
-                mt="15px"
-              >
-                Pi gwo kominote dijital<br />Devlopè Ayisyen<br />nan mond lan.
+              <Heading fontSize="52px" mt="15px">
+                Pi gwo kominote dijital
+                <br />
+                Devlopè Ayisyen
+                <br />
+                nan mond lan.
               </Heading>
             </Flex>
 
-            <Flex className="section fp-noscroll" align="center" justify="center" textAlign="center">
-              <Heading fontSize="32px">
-                Valè nou ak Vizyon nou
-              </Heading>
+            <Flex
+              className="section fp-noscroll"
+              align="center"
+              justify="center"
+              textAlign="center"
+            >
+              <Heading fontSize="32px">Valè nou ak Vizyon nou</Heading>
 
-              <Flex
-                mt="50px"
-                textAlign="center"
-                columnGap="120px"
-              >
+              <Flex mt="50px" textAlign="center" columnGap="120px">
                 <Flex
                   direction="column"
                   w="350px"
                   align="center"
                   justify="center"
                 >
-                  <Heading>
-                    Ayiti Avantou
-                  </Heading>
+                  <Heading>Ayiti Avantou</Heading>
 
                   <Text mt="15px">
-                    Nou fè efò pou nou anplifye travay tout Devlopè ak Kominote Ayisyen yo sou fòm dijital ak fizik. Sitou sa ki baze an Ayiti yo.
+                    Nou fè efò pou nou anplifye travay tout Devlopè ak Kominote
+                    Ayisyen yo sou fòm dijital ak fizik. Sitou sa ki baze an
+                    Ayiti yo.
                   </Text>
                 </Flex>
 
@@ -74,33 +79,29 @@ const Home = () => {
                   align="center"
                   justify="center"
                 >
-                  <Heading>
-                    Pa gen Chèf
-                  </Heading>
+                  <Heading>Pa gen Chèf</Heading>
 
                   <Text mt="15px">
-                    Eksepte pou moderatè kominote yo, pa gen dirijan. Manm kominote yo se lidè yo!
+                    Eksepte pou moderatè kominote yo, pa gen dirijan. Manm
+                    kominote yo se lidè yo!
                   </Text>
                 </Flex>
               </Flex>
 
-              <Flex
-                mt="100px"
-                textAlign="center"
-                columnGap="120px"
-              >
+              <Flex mt="100px" textAlign="center" columnGap="120px">
                 <Flex
                   direction="column"
                   w="350px"
                   align="center"
                   justify="center"
                 >
-                  <Heading>
-                    Nou Grandi Ansanm
-                  </Heading>
+                  <Heading>Nou Grandi Ansanm</Heading>
 
                   <Text mt="15px">
-                    Nou tout gen potansyel, men nou tout pa jwenn menm opotinite. Nou espere ankouraje egalite epi pemet li pi fasil pou Devlopè Ayisyen yo aprann youn de lot atravè evènman kominote nou yo dirije.
+                    Nou tout gen potansyel, men nou tout pa jwenn menm
+                    opotinite. Nou espere ankouraje egalite epi pemet li pi
+                    fasil pou Devlopè Ayisyen yo aprann youn de lot atravè
+                    evènman kominote nou yo dirije.
                   </Text>
                 </Flex>
 
@@ -110,22 +111,29 @@ const Home = () => {
                   align="center"
                   justify="center"
                 >
-                  <Heading>
-                    Totalman Ouvè
-                  </Heading>
+                  <Heading>Totalman Ouvè</Heading>
 
                   <Text mt="15px">
-                    Platfòm nou ak zouti nou yo open-source, epi devlopè volontè nou yo ouvè a piblik la. Se manm nou yo ki enfliyanse ak vote sou desizyon nou yo.              </Text>
+                    Platfòm nou ak zouti nou yo open-source, epi devlopè volontè
+                    nou yo ouvè a piblik la. Se manm nou yo ki enfliyanse ak
+                    vote sou desizyon nou yo.{" "}
+                  </Text>
                 </Flex>
               </Flex>
             </Flex>
 
-            <Flex className="section fp-noscroll" align="center" justify="center" textAlign="center">
-              <Heading fontSize="32px">
-                Rejwenn Nou!
-              </Heading>
+            <Flex
+              className="section fp-noscroll"
+              align="center"
+              justify="center"
+              textAlign="center"
+            >
+              <Heading fontSize="32px">Rejwenn Nou!</Heading>
 
-              <Text mt="15px">N ap tann ou pou nou rive grandi a on pwen kote nou bezwen kreye plis kominote nan sektè teknoloji a</Text>
+              <Text mt="15px">
+                N ap tann ou pou nou rive grandi a on pwen kote nou bezwen kreye
+                plis kominote nan sektè teknoloji a
+              </Text>
 
               <Flex
                 mt="50px"
@@ -146,15 +154,13 @@ const Home = () => {
                   description="Sou Discord, nou enteraji sou plizyè sijè teknolojik, nou vote sou desizyon enpotan kominote a, nou kreye epi patisipe nan challenge, ak anpil lot aktivite anko."
                   link="https://discord.gg/tDVzKz45"
                 />
-
               </Flex>
             </Flex>
           </ReactFullpage.Wrapper>
-        )
-        }
+        )}
       />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
