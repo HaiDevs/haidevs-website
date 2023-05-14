1dbf2237b96d53c03249127f927cc3a740b4b090
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-05-13 21:28:58 -0400

    add post tags &  update link

--- components/Post.tsx
+++ components/Post.tsx
@@ -3,53 +3,49 @@ import {
 	useColorModeValue,
 	Box,
 	Text,
-	Link,
 	Img,
-	Button,
-	Card,
-	CardBody,
-	CardFooter,
-	Stack
+	Card
 } from "@chakra-ui/react";
 
 import React from "react";
-import BlogTags from "./PostCategory";
-import { IPostProps } from "types/types";
 
-export default function Post(post: IPostProps) {
+import { IPost } from "interfaces/Posts";
+import PostTags from "./PostTags";
+import CustomLink from "./common/CustomLink";
+
+export default function Post(post: IPost) {
 	return (
 		<Card
 			direction={{ base: "column", sm: "row" }}
 			bg="black"
 			alignItems={"center"}
 			padding={1}
-			boxShadow={" 0 3px 10px rgb(0 0 0 / 0.2)"}
 		>
 			<Box paddingRight={{ base: 0, sm: 35, md: 50 }}>
-				<Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+				<CustomLink href={`/blog/${post.slug}`}>
 					<Img
 						objectFit="cover"
 						width={{ base: "100%", sm: 350, md: 350 }}
 						height={{ base: "auto", md: 180 }}
-						src={post.image}
+						src={post.cover_image}
 						alt="card-image"
 						borderRadius={10}
 					/>
-				</Link>
+				</CustomLink>
 			</Box>
 			<Box pt={{ base: 0, xs: 5 }} width={{ base: "100%", md: 650 }}>
-				<BlogTags category={post.category} />
-				<Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+				<PostTags tags={post.tags} />
+				<CustomLink href={`/blog/${post.slug}`}>
 					<Heading
 						size={{ base: "none", sm: "sm", md: "md", lg: "lg" }}
 						color={useColorModeValue("white", "white.800")}
 					>
 						{post.title}
 					</Heading>
-					<Text py="5" color={useColorModeValue("gray", "gray.800")}>
-						{post.date}
-					</Text>
-				</Link>
+				</CustomLink>
+				<Text py="5" color={"gray"}>
+					{post.date}
+				</Text>
 			</Box>
 		</Card>
 	);
